80d1c6fd420f96f29a3f7f4f9eeeb48fffe7194c
koron4ik <42875321+koron4ik@users.noreply.github.com>
2018-12-14 19:14:03 +0300

    Initial Commit

---
+++ RateGoods/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RateGoods
+//
+//  Created by Vadim on 12/14/18.
+//  Copyright © 2018 Koronchik. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
